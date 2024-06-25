0d38b7bc48f01b370239c8fa8394cb454507bf6f
Uria Medalia <uriamedalia@gmail.com>
2024-06-26 01:52:10 +0300

    add current daily lesson, base on sefaria caleder

---
+++ client/src/ApiService/Requests/useDailySource.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @cspell/spellchecker */
+import { IDailyLessons, ILessonSource } from '@ApiService/Interfaces/IDailySource';
+import { DAILY_LESSONS_KEY, DAILY_LESSON_SOURCE_KEY } from '@CommonConstants';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const SEFARIA_URL = 'https://www.sefaria.org/api';
+const DEFAULT_DATE = new Date();
+
+export const axiosClient = axios.create({
+  baseURL: `${SEFARIA_URL}`,
+  withCredentials: true,
+});
+
+const getDailyLessons = async (date: Date): Promise<IDailyLessons> => {
+  const dateFilter = `?year=${date.getFullYear()}&month=${date.getMonth() + 1}&day=${date.getDate()}`;
+
+  return fetch(`${SEFARIA_URL}/calendars${dateFilter}`).then((res) => res.json());
+};
+
+const getBibleDailyLessonSource = async (tref?: string): Promise<string[][]> => {
+  const res = await fetch(`${SEFARIA_URL}/v3/texts/${tref}?version=hebrew`).then(
+    (res) => res.json() as Promise<ILessonSource>
+  );
+
+  return res?.versions?.[0]?.text;
+};
+
+export const useDailyLessons = (date: Date) => {
+  return useQuery<unknown, unknown, IDailyLessons>({
+    queryKey: [DAILY_LESSONS_KEY, date],
+    queryFn: () => getDailyLessons(date),
+    initialData: {},
+    staleTime: 0,
+    cacheTime: 0,
+  });
+};
+
+export const useBibleDailyLessonSource = (tref?: string) => {
+  return useQuery<string[][], unknown, string[][]>({
+    queryKey: [DAILY_LESSON_SOURCE_KEY, tref],
+    queryFn: () => getBibleDailyLessonSource(tref),
+    initialData: [],
+    staleTime: 0,
+    cacheTime: 0,
+    enabled: !!tref,
+  });
+};
+export const useDailyLessonSource = (date = DEFAULT_DATE) => {
+  const {
+    data: dailyLessons,
+    isLoading: isLoadingLessons,
+    error: loadLessonsError,
+  } = useDailyLessons(date);
+
+  const bibleDailyLessons = dailyLessons?.calendar_items?.find(
+    (item) => item.title.en === 'Tanakh Yomi'
+  );
+
+  const {
+    data: dailyLessonSource,
+    isLoading: isLoadingSource,
+    error: loadSourceError,
+  } = useBibleDailyLessonSource(bibleDailyLessons?.ref);
+
+  return {
+    isLoading: isLoadingLessons || isLoadingSource,
+    error: loadLessonsError ?? loadSourceError,
+    data: dailyLessonSource,
+  };
+};
--- client/src/Pages/Bible/Bible.tsx
+++ client/src/Pages/Bible/Bible.tsx
@@ -1,5 +1,31 @@
+import { useDailyLessonSource } from '@ApiService/Requests/useDailySource';
+import DOMPurify from 'dompurify';
+import style from './Bible.module.scss';
+
+const SafeHebrewText = ({ htmlContent }: { htmlContent: string | Node }) => {
+  const safeHTML = DOMPurify.sanitize(htmlContent);
+
+  return <div dangerouslySetInnerHTML={{ __html: safeHTML }} />;
+};
+
 const Bible = () => {
-  return <div>Home Page for bible learn!</div>;
+  const { data } = useDailyLessonSource();
+
+  return (
+    <>
+      <h2 className={style['daily-lesson-header']}>הלימוד היומי</h2>
+      <div className={style['lesson-container']}>
+        <div className={style['lesson-source-container']}>
+          {data.reverse().map((episode, index) => (
+            <div key={index} className={style['episode']}>
+              <SafeHebrewText htmlContent={episode.join('  ')} />
+            </div>
+          ))}
+        </div>
+        <div className={style['lesson-audio']}>{/** TODO: add spotify player & calender */}</div>
+      </div>
+    </>
+  );
 };
 
 export default Bible;
